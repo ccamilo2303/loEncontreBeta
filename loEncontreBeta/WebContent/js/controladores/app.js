9512f83d5db626921177b8da60a97a14181e4830
crick <crick@crick-PC>
2016-06-05 21:27:13 -0500

    Se hacen cambios en la landin page

--- loEncontreBeta/WebContent/js/controladores/app.js
+++ loEncontreBeta/WebContent/js/controladores/app.js
@@ -1,18 +1,45 @@
 var modulo = angular.module('loEncontreApp',['ngRoute'])
-            .config(['$routeProvider', function($routeProvider){
-                $routeProvider
-                .when('/', {
-                    templateUrl: '../templates/landinPage.html',
-                    controller: 'main'
-                  }).
-                  otherwise({
-                    redirectTo: '/addOrder'
-                  });
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: '../templates/landinPage.html',
+		controller: 'main'
+	}).
+	otherwise({
+		redirectTo: '/addOrder'
+	});
 
-            }]);
+}]);
 
-modulo.controller('main', function($scope){
-	
+modulo.controller('main', function($scope,$http){
+	$scope.load = false;
+	$scope.labelBoton = 'Buscar';
+	$scope.validarEmail = function(){
+		var email = $scope.emailLogin;
+		if(email == undefined){
+			return;
+		}
+		$scope.load = true;
+		$scope.labelBoton = 'Buscando';
+
+		$http.get("../rest/encontrarDocumentos/"+$scope.emailLogin)
+		.then(function(data){
+
+			console.log(data.data);
+			$scope.load = false;
+			$scope.labelBoton = 'Buscar';
+			if(data.data.msg == true){
+				new swal({   title: "Error!",   text: data.data.message,   type: "error",   confirmButtonText: "Cerrar" });
+			}
+		});
+	}
 })
 
+
+function moveTo(to){
+	console.log(to+' ---- ');
+	$('html, body').animate({
+		scrollTop: $("#"+to+"").offset().top
+	}, 800);
+}
 
---
+++ loEncontreBeta/src/com/loEncontre/REST/FindAllDocuments.java
@@ -0,0 +1,31 @@
+package com.loEncontre.REST;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONObject;
+
+import com.loEncontre.DAO.FindAllDocumentDAO;
+
+@Path("encontrarDocumentos")
+public class FindAllDocuments {
+
+	private FindAllDocumentDAO findDocuments = FindAllDocumentDAO.getInstance();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{email}") 
+	public String insertDocument(@PathParam("email") String email){
+		String retorno = findDocuments.findDocuments(email);
+		JSONObject validacion = new JSONObject(retorno);
+		if(validacion.getBoolean("msg") == true){
+			validacion.put("message", "No se encontraron documentos para este email");
+		}
+		System.out.println(validacion.toString());
+		return validacion.toString();
+	}
+	
+}
